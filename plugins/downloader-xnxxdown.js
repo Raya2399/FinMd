419d881af794cc17f4c6215da8b4134f7492dc2c
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2025-01-16 20:17:01 +0700

    Update downloader-xnxxdown.js

--- plugins/downloader-xnxxdown.js
+++ plugins/downloader-xnxxdown.js
@@ -16,4 +16,7 @@ conn.sendMessage(m.chat, { video: { url: hasil.result.url }, fileName: 'xnxx.mp4
 }
 handler.command = handler.help = ['xnxxdown'];
 handler.tags = ['internet'];
+handler.limit = true;
+handler.premium = true;
+
 module.exports = handler;
