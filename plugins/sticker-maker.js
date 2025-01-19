0d7862c294cfb8706d02f65f5cbb431269389bf1
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2025-01-19 12:23:35 +0700

    brat vid

--- plugins/sticker-maker.js
+++ plugins/sticker-maker.js
@@ -18,6 +18,8 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
     let res;
     if (command === 'brat') {
         res = `https://api.botcahx.eu.org/api/maker/brat?text=${encodeURIComponent(text.substring(0, 151))}&apikey=${btc}`;
+    } else if (command === 'brat2' || command === 'bratgif' || command === 'bratvid') {
+        res = `https://api.botcahx.eu.org/api/maker/brat-video?text=${encodeURIComponent(text.substring(0, 151))}&apikey=${btc}`;
     } else if (command === 'ttp') {
         res = `https://api.botcahx.eu.org/api/maker/ttp?text=${encodeURIComponent(text.substring(0, 151))}&apikey=${btc}`;
     } else if (command === 'attp') {
@@ -29,15 +31,20 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
     try {
         const response = await fetch(res);
         const buffer = await response.buffer();
-        const stiker = await sticker5(buffer, false, global.packname, global.author);
-        await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m);
+
+        if (command === 'bratgif' || command === 'brat2' || command === 'bratvid') {
+            await conn.sendVideoAsSticker(m.chat, buffer, m, { packname: global.packname, author: global.author });
+        } else {
+            const stiker = await sticker5(buffer, false, global.packname, global.author);
+            await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m);
+        }
     } catch (e) {
         console.error(e);
         await conn.sendFile(m.chat, err, 'error.webp', '', m);
     }
 };
 
-handler.command = handler.help = ['brat', 'ttp', 'attp'];
+handler.command = handler.help = ['brat', 'brat2', 'bratgif', 'bratvid', 'ttp', 'attp'];
 handler.tags = ['sticker'];
 handler.limit = true;
 
