0fc27cabe9bbb61f4a80b6934f9be60e3b321ce7
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2025-02-22 17:34:40 +0700

    Update tools-rvo.js

--- plugins/tools-rvo.js
+++ plugins/tools-rvo.js
@@ -1,37 +1,18 @@
-/* 
-Script By Reelly XD
-  • YT: ReellyXD
-  • IG: reellyxd
-Buy Script? 
-  • WA: +62 857-0436-85323
-  • TELE: t.me/rely_xd
-  • Github: github.com/ReellyXD
-*/
-
-const { downloadContentFromMessage } = require('@adiwajshing/baileys')
-
-let handler = async (m, { conn }) => {
-  if (!m.quoted) throw 'Reply gambar/video yang ingin Anda lihat'
-  if (m.quoted.mtype !== 'viewOnceMessageV2') throw 'Ini bukan pesan viewonce.'
-  let msg = m.quoted.message
-  let type = Object.keys(msg)[0]
-  let media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video')
-  let buffer = Buffer.from([])
-  for await (const chunk of media) {
-    buffer = Buffer.concat([buffer, chunk])
-  }
-  if (/video/.test(type)) {
-    return conn.sendFile(m.chat, buffer, 'media.mp4', msg[type].caption || '', m)
-  } else if (/image/.test(type)) {
-    return conn.sendFile(m.chat, buffer, 'media.jpg', msg[type].caption || '', m)
-  }
+ let handler = async (m, { conn }) => {
+	let q = m.quoted ? m.quoted : m
+	try {
+	let media = await q.download?.()
+	await conn.sendFile(m.chat, media, null, '', m)
+	} catch (e) {
+      m.reply('Media gagal dimuat!')
+	}
 }
 
-handler.help = ['readvo']
+handler.help = ['readviewonce']
 handler.tags = ['tools']
 handler.command = ['readviewonce', 'read', 'rvo', 'liat', 'readvo']
 handler.premium = false
 handler.register = false
-handler.limit = true
+handler.fail = null
 
 module.exports = handler
