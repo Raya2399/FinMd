f8724b873261f1a5ed0fc31304b1b602be23bbaa
Tio <94074076+BOTCAHX@users.noreply.github.com>
2025-05-26 15:01:13 +0700

    ga jalan:v

--- plugins/_autosholat.js
+++ plugins/_autosholat.js
@@ -1,56 +1,47 @@
-const fetch = require('node-fetch');
-
 module.exports = {
-    before: async function (m) {
-        this.autosholat = this.autosholat || {};
-        let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender;
-        let id = m.chat;
-
-        let jadwalSholat = {};
-        try {
-            const kota = 'jakarta'; //ganti dengan Daerah masing-masing
-            const response = await fetch(`https://api.botcahx.eu.org/api/tools/jadwalshalatv2?kota=${kota}&apikey=${btc}`);
-            const data = await response.json();
-            if (data.status === true) {
-                jadwalSholat = data.result;
-            } else {
-                console.error(data);
-                return false;
-            }
-        } catch (error) {
-            console.error(error);
-            return false;
+before: async function (m) {
+    this.autosholat = this.autosholat || {}
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
+    let id = m.chat
+    let jadwalSholat = {
+            Fajr: "04:49",
+            Sunrise: "06:04",
+            Dhuhr: "12:06",
+            Asr: "15:21",
+            Sunset: "18:08",
+            Maghrib: "18:08",
+            Isha: "19:38",
+            Imsak: "04:39",
+            Midnight: "00:06",
+            Firstthird: "22:07",
+            Lastthird: "02:06"
         }
+    const date = new Date((new Date).toLocaleString("en-US", {
+        timeZone: "Asia/Jakarta"
+    }));
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const timeNow = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+    let isActive = Object.values(this.autosholat).includes(true);
+    if (id in this.autosholat && isActive) {
+        return false
+    }
 
-        const now = new Date().toLocaleString("en-US", { timeZone: "Asia/Jakarta" });
-        const currentTime = new Date(now);
-        const timeNow = `${currentTime.getHours().toString().padStart(2, "0")}:${currentTime.getMinutes().toString().padStart(2, "0")}`;
-
-        let isActive = Object.values(this.autosholat).some(entry => entry[0]);
-        if (id in this.autosholat && isActive) {
-            return false;
+    for (const [sholat, waktu] of Object.entries(jadwalSholat)) {
+        if (timeNow === waktu && !(id in this.autosholat)) {
+            let caption = `Hai kak @${who.split`@`[0]},\nWaktu *${sholat}* telah tiba, ambilah air wudhu dan segeralah shalat.\n\n*${waktu}*\n_untuk wilayah Jakarta dan sekitarnya._`
+            this.autosholat[id] = [
+                this.reply(m.chat, caption, null, {
+                    contextInfo: {
+                        mentionedJid: [who]
+                    }
+                }),
+                setTimeout(() => {
+                    delete this.autosholat[id]
+                }, 57000)
+            ]
         }
-
-        for (const [sholat, waktu] of Object.entries(jadwalSholat)) {
-            if (timeNow === waktu && !(id in this.autosholat)) {
-                const audioUrl = sholat === 'Fajr' ? 'https://i.supa.codes/FV-Wx3' : 'https://i.supa.codes/PeOcbg';
-                const caption = `Hai kak @${who.split`@`[0]},\nWaktu *${sholat}* telah tiba, ambilah air wudhu dan segeralah shalat.\n\n*${waktu}*\n_untuk wilayah Jakarta dan sekitarnya._`;
-
-                this.autosholat[id] = [
-                    Promise.all([
-                        this.sendFile(m.chat, audioUrl, 'adzan.mp3', null, m),
-                        this.reply(m.chat, caption, null, {
-                            contextInfo: {
-                                mentionedJid: [who]
-                            }
-                        })
-                    ]),
-                    setTimeout(() => {
-                        delete this.autosholat[id];
-                    }, 57000)
-                ];
-            }
-        }
-    },
-    disabled: false
-};
+    }
+},
+disabled: false
+}
