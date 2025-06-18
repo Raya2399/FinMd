c331a54f1e0bfd28ddf3432c368251814773f43b
Tio <94074076+BOTCAHX@users.noreply.github.com>
2025-06-19 02:46:37 +0700

    tidak jadi

--- plugins/sticker-maker.js
+++ plugins/sticker-maker.js
@@ -1,10 +1,8 @@
 const { sticker5 } = require('../lib/sticker');
 const fs = require('fs');
 const fetch = require('node-fetch');
-const sharp = require('sharp');
-const { Sticker } = require('wa-sticker-formatter');
 
-let handler = async (m, { conn, args, text, usedPrefix, command }) => {
+const handler = async (m, { conn, args, text, usedPrefix, command }) => {
     text = text 
         ? text 
         : m.quoted && m.quoted.text 
@@ -20,9 +18,7 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
     let res;
     if (command === 'brat') {
         res = `https://api.botcahx.eu.org/api/maker/brat?text=${encodeURIComponent(text.substring(0, 151))}&apikey=${btc}`;
-    } else if (command === 'brat2' || command === 'bratgif') {
-        res = `https://api.botcahx.eu.org/api/maker/brat-video?text=${encodeURIComponent(text.substring(0, 151))}&apikey=${btc}`;
-    } else if (command === 'bratvid') {
+    } else if (command === 'brat2' || command === 'bratgif' || command === 'bratvid') {
         res = `https://api.botcahx.eu.org/api/maker/brat-video?text=${encodeURIComponent(text.substring(0, 151))}&apikey=${btc}`;
     } else if (command === 'ttp') {
         res = `https://api.botcahx.eu.org/api/maker/ttp?text=${encodeURIComponent(text.substring(0, 151))}&apikey=${btc}`;
@@ -36,15 +32,10 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
         const response = await fetch(res);
         const buffer = await response.buffer();
 
-        if (command === 'bratvid') {
+        if (command === 'bratgif' || command === 'brat2' || command === 'bratvid') {
             await conn.sendVideoAsSticker(m.chat, buffer, m, { packname: global.packname, author: global.author });
         } else {
-            let decodedBuffer = await sharp(buffer)
-                .resize({ width: 512, height: 512, fit: 'contain', background: { r: 255, g: 255, b: 255, alpha: 1 } })
-                .toFormat('png')
-                .toBuffer();
-            
-            const stiker = await createWebp(decodedBuffer, global.packname, global.author);
+            const stiker = await sticker5(buffer, false, global.packname, global.author);
             await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m);
         }
     } catch (e) {
@@ -52,16 +43,6 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
         await conn.sendFile(m.chat, err, 'error.webp', '', m);
     }
 };
-
-async function createWebp(buffer, packName, authorName, quality = 100) {
-    let metadata_sticker = {
-        type: 'full',
-        pack: packName || global.packname,
-        author: authorName || global.author,
-        quality
-    };
-    return (new Sticker(buffer, metadata_sticker)).toBuffer();
-}
 
 handler.command = handler.help = ['brat', 'brat2', 'bratgif', 'bratvid', 'ttp', 'attp'];
 handler.tags = ['sticker'];
