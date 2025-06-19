18a447b0e0616343dfa10aecb79fe7a48d37005d
Tio <94074076+BOTCAHX@users.noreply.github.com>
2025-06-19 10:25:33 +0700

    Update brat image, attp

--- plugins/sticker-maker.js
+++ plugins/sticker-maker.js
@@ -1,4 +1,3 @@
-const { sticker5 } = require('../lib/sticker');
 const fs = require('fs');
 const fetch = require('node-fetch');
 
@@ -18,7 +17,9 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
     let res;
     if (command === 'brat') {
         res = `https://api.botcahx.eu.org/api/maker/brat?text=${encodeURIComponent(text.substring(0, 151))}&apikey=${btc}`;
-    } else if (command === 'brat2' || command === 'bratgif' || command === 'bratvid') {
+    } else if (command === 'brat2' || command === 'bratgif') {
+        res = `https://api.botcahx.eu.org/api/maker/brat-video?text=${encodeURIComponent(text.substring(0, 151))}&apikey=${btc}`;
+    } else if (command === 'bratvid') {
         res = `https://api.botcahx.eu.org/api/maker/brat-video?text=${encodeURIComponent(text.substring(0, 151))}&apikey=${btc}`;
     } else if (command === 'ttp') {
         res = `https://api.botcahx.eu.org/api/maker/ttp?text=${encodeURIComponent(text.substring(0, 151))}&apikey=${btc}`;
@@ -32,15 +33,16 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
         const response = await fetch(res);
         const buffer = await response.buffer();
 
-        if (command === 'bratgif' || command === 'brat2' || command === 'bratvid') {
+        if (command === 'attp') {
+            await conn.sendFile(m.chat, buffer, 'sticker.webp', '', m);
+        } else if (command === 'bratvid' || command === 'brat2' || command === 'bratgif') {
             await conn.sendVideoAsSticker(m.chat, buffer, m, { packname: global.packname, author: global.author });
         } else {
-            const stiker = await sticker5(buffer, false, global.packname, global.author);
-            await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m);
+            await conn.sendImageAsSticker(m.chat, buffer, m, { packname: global.packname, author: global.author });
         }
     } catch (e) {
         console.error(e);
-        await conn.sendFile(m.chat, err, 'error.webp', '', m);
+        await conn.sendImageAsSticker(m.chat, err, m, { packname: global.packname, author: global.author });
     }
 };
 
