bfedd5e29611ccdfc001c2a1e51d2ad4626277e9
Raya2399 <102332034+Raya2399@users.noreply.github.com>
2026-02-27 22:02:32 +0800

    Update rpg-mancing.js

--- plugins/rpg-mancing.js
+++ plugins/rpg-mancing.js
@@ -1,6 +1,6 @@
-let {
-	MessageType
-} = require('@adiwajshing/baileys');
+const { loadBaileys } = require('../baileys-loader.mjs');
+
+let baileys;
 
 let wm = global.wm;
 let handler = async (m, {
@@ -8,6 +8,12 @@ let handler = async (m, {
 	usedPrefix,
 	owner
 }) => {
+  if (!baileys) {
+    baileys = await loadBaileys();
+  }
+
+  const { MessageType } = baileys;
+  
 		let lastFishingTime = global.db.data.users[m.sender].lastmancing || 0;
 		let timeDiff = Date.now() - lastFishingTime;
 		let remainingTime = 180000 - timeDiff; 
@@ -22,7 +28,7 @@ let handler = async (m, {
 				let paus = Math.floor(Math.random() * 2);
 				let kepiting = Math.floor(Math.random() * 27);
 
-				let imageUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYJHYMaM4vVzvHstniD2n_oL7L-gjfka6NSQ&s';
+				let imageUrl = 'https://api.botcahx.eu.org/api/tools/get-upload?id=f/arit56zv.jpg';
 				let totalCatch = nila + bawal + ikan + lele + udang + paus + kepiting;
 
 				let mcng = `•  *Hasil Mancing:*
@@ -72,7 +78,7 @@ let handler = async (m, {
 handler.help = ['mancing'];
 handler.tags = ['rpg'];
 handler.command = /^(mancing|memancing)$/i;
-
+handler.rpg = true
 module.exports = handler;
 
 function formatTime(ms) {
